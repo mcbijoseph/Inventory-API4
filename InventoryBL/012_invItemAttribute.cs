f0fb381909989265e1a1b9b11e4c63e5a4687993
Nico Romano <Nico Romano@LAPTOP-LAIODKR7>
2020-01-23 11:40:05 +0800

    GEA

---
+++ InventoryBL/012_invItemAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using Inventory.DAL;
+using Inventory_Domain_Layer;
+
+namespace InventoryBL
+{
+   
+}
