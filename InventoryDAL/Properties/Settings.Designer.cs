4ce6d301e59c78c79eba988a95fb6bdecb7ff047
MCBInico <nico234.lds234@gmail.com>
2020-02-05 14:05:42 +0800

    qwerty

--- InventoryDAL/Properties/Settings.Designer.cs
+++ InventoryDAL/Properties/Settings.Designer.cs
@@ -45,7 +45,7 @@ namespace InventoryDAL.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("True")]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
         public bool isTest {
             get {
                 return ((bool)(this["isTest"]));
