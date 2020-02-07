3c419ca3f35cad845b7422647c9e9d3356f81e1f
MCBInico <nico234.lds234@gmail.com>
2020-02-07 11:52:07 +0800

    halhkhjeaoihr

---
+++ Inventory API4/Controllers/RawControllers/ItemIssuanceController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using InventoryBL;
+
+namespace Inventory_API4.Controllers.RawControllers
+{
+#pragma warning disable 1591
+    public class ItemIssuanceController : ApiController
+    {
+        private IvwItemProjectQuantityBL _Issuance;
+        public ItemIssuanceController(IvwItemProjectQuantityBL issuance)
+        {
+            this._Issuance = issuance;
+        }
+
+        [Route("issuance/item/{ProjectID}")]
+        public IHttpActionResult GetItems(int ProjectID)
+        {
+            return Ok(_Issuance.GetRecords(ProjectID));
+        }
+    }
+}
