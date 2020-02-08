ea7bc7e23eea903852f580a17b2fac9a9211d1e4
MCBInico <nico234.lds234@gmail.com>
2020-02-08 11:25:36 +0800

    new ups api

---
+++ Inventory API4/Controllers/ReferenceApi/ENGProjectListController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using Inventory_Domain_Layer;
+
+namespace Inventory_API4.Controllers.ReferenceApi
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class ENGProjectListController : ApiController
+    {
+
+        public IHttpActionResult Get()
+        {
+            return Ok(ApiReferenceHolder.projectArray);
+        }
+    }
+}
