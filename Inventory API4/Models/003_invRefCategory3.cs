8fa151aa773dac55839d792a523441f0a8e3f2aa
mcbijoseph <57780263+mcbijoseph@users.noreply.github.com>
2020-02-26 11:23:07 +0800

    item updates

--- Inventory API4/Models/003_invRefCategory3.cs
+++ Inventory API4/Models/003_invRefCategory3.cs
@@ -15,6 +15,7 @@ namespace Inventory_API4.Models
         public int Cat2ID_002 { get; set; }
         [Required]
         public string Name { get; set; }
-        public List<_005_invRefPropertyName2Domain> Property2 { get; set; }
+       // public List<_005_invRefPropertyName2Domain> Property2 { get; set; }
+       public bool hasAttribute { get; set; }
     }
 }
