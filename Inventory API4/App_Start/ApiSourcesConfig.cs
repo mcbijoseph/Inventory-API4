1a4ce4d7d6dfa65ff243622ccfe1e4e078983843
mcbijoseph <57780263+mcbijoseph@users.noreply.github.com>
2020-03-24 13:54:59 +0800

    api sources only

--- Inventory API4/App_Start/ApiSourcesConfig.cs
+++ Inventory API4/App_Start/ApiSourcesConfig.cs
@@ -11,9 +11,17 @@ namespace Inventory_API4.App_Start
         public static int requestCount = 0;
         public static void Config()
         {
-            Inventory_API4.Models.ApiReferenceHolder.projectArray = Newtonsoft.Json.JsonConvert.DeserializeObject<JArray>(new Helper.SynchronousRequest("http://124.105.198.3:94/api/Projects").HttpRequest());
-            Inventory_API4.Models.ApiReferenceHolder.hrmsArray = Newtonsoft.Json.JsonConvert.DeserializeObject<JArray>(new Helper.SynchronousRequest("http://124.105.198.3:92/api/PersonInformation").HttpRequest());
+            ////ONLINE
+            //Inventory_API4.Models.ApiReferenceHolder.projectArray = Newtonsoft.Json.JsonConvert.DeserializeObject<JArray>(new Helper.SynchronousRequest("http://124.105.198.3:94/api/Projects").HttpRequest());
+            //Inventory_API4.Models.ApiReferenceHolder.hrmsArray = Newtonsoft.Json.JsonConvert.DeserializeObject<JArray>(new Helper.SynchronousRequest("http://124.105.198.3:92/api/PersonInformation").HttpRequest());
+            //Inventory_API4.Models.ApiReferenceHolder.supplierArray = Newtonsoft.Json.JsonConvert.DeserializeObject<JArray>(new Helper.SynchronousRequest("http://124.105.198.3:99/api/supplier/0").HttpRequest());
+
+            //LOCAL
+            Inventory_API4.Models.ApiReferenceHolder.projectArray = Newtonsoft.Json.JsonConvert.DeserializeObject<JArray>(new Helper.SynchronousRequest("http://192.168.1.100:94/api/Projects").HttpRequest());
+            Inventory_API4.Models.ApiReferenceHolder.hrmsArray = Newtonsoft.Json.JsonConvert.DeserializeObject<JArray>(new Helper.SynchronousRequest("http://192.168.1.100:92/api/PersonInformation").HttpRequest());
             Inventory_API4.Models.ApiReferenceHolder.supplierArray = Newtonsoft.Json.JsonConvert.DeserializeObject<JArray>(new Helper.SynchronousRequest("http://192.168.1.100:99/api/supplier/0").HttpRequest());
+
+
             requestCount++;
         }
     }
